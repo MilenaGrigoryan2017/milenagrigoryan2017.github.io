65f9f44d267a9f931f06d65753dbab427674f3f1
MilenaGrigoryan2017 <160655241+MilenaGrigoryan2017@users.noreply.github.com>
2024-02-20 23:35:28 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+let day=document.querySelector(".day");
+let hours=document.querySelector(".hours");
+let minutes=document.querySelector(".minutes");
+let seconds=document.querySelector(".seconds");
+function countDown(){
+let d=new Date();
+// let year=d.getFullYear()+1;
+let newYear=new Date(2024,1,23,0,0,0);
+
+let dMiliseconds=d.getTime();
+let newYearMiliseconds=newYear.getTime();
+
+let defference=newYearMiliseconds-dMiliseconds;
+// 1*24*60*60*1000
+let days=Math.floor(defference/(1000*60*60*24));
+
+let hoursd=Math.floor((defference/(1000*60*60))%24);
+
+let minutesd=Math.floor((defference/(1000*60))%60);
+
+let secondsd=Math.floor((defference/1000)%60);
+
+day.innerHTML=days;
+hours.innerHTML=((hoursd<10)?"0":"") + hoursd;
+minutes.innerHTML=((minutesd<10)?"0":"") + minutesd;
+seconds.innerHTML=((secondsd<10)?"0":"") + secondsd;
+}
+
+setInterval(countDown,1000);
+let ssss=new Date();
+console.log(ssss)
+
+
+
